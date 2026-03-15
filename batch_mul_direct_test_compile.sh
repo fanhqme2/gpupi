3778777cd5e70d3ed5255e674a541d4e37a04e48
Haoqiang Fan <fhq@dexmal.com>
2026-03-15 16:10:08 +0800

    Remove batch_mul_direct files

--- batch_mul_direct_test_compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Compile batch_mul_direct_test.cu and batch_mul_direct.cu into batch_mul_direct_test
-
-nvcc -O3 -o batch_mul_direct_test batch_mul_direct_test.cu batch_mul_direct.cu -lgmp
-
